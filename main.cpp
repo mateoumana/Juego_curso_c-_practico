d42feb226b6e1724d4fd37981c510d762596926a
Mateo Umana <mateo.umana@mail.escuelaing.edu.co>
2024-03-10 18:29:35 +0100

    add como colaborador el otro PC

---
+++ main.cpp
@@ -0,0 +1,13 @@
+void printMap(int *heroPos, int *lifes, char *mapWorld){
+    for(int i ;i < sizeof(mapWorld);i++){
+
+    }
+}
+
+void main(){
+    int heroPos;
+    int lifes;
+    char mapWorld[10];
+    printMap(&heroPos, &lifes, mapWorld);
+
+}
