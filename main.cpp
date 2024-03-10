2861d775f515a271ac59b0be6e0aff80d5305251
elmaxwell <matius1406@gmail.com>
2024-03-10 19:00:32 +0100

    agregando .ignore

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
+#include <iostream>
+using namespace std;
+
 void printMap(int *heroPos, int *lifes, char *mapWorld){
     for(int i ;i < sizeof(mapWorld);i++){
-
+        cout << *(mapWorld + i) << endl;
     }
 }
 
-void main(){
-    int heroPos;
-    int lifes;
-    char mapWorld[10];
+int main(){
+    int heroPos = 1;
+    int lifes = 3;
+    char mapWorld[10] = {'I','I','I','I','I','I','I','I','I','I'};
+    cout << "Hola Mateo" << endl;
     printMap(&heroPos, &lifes, mapWorld);
-
+    return 0;
 }
